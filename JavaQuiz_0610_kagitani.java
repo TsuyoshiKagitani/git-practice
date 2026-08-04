102cd6b843fd567bee248b9fe3ec5e8526a7edd1
TsuyoshiKagitani <127452212+TsuyoshiKagitani@users.noreply.github.com>
2026-08-04 21:35:04 +0900

    demo

---
+++ JavaQuiz_0610_kagitani.java
@@ -0,0 +1,8 @@
+public class JavaQuiz_0610_kagitani {
+	public static void main(String[] args){
+		Item2 i1 = new Item2("コーヒー", 120);
+		Item2 i2 = new Item2("オレンジジュース", 150);
+		i1.display();
+		i2.display();
+	}
+}
